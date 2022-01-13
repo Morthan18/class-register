d19a6450fd1f9493153f1ebe41ccb6a7534d4583
Yarecky <arek.erduk@gmail.com>
2022-01-13 20:49:16 +0100

    theme

---
+++ school-management/school-management/Models/MyProfile.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace school_management.Models
+{
+    public class MyProfile
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        
+        [Column(TypeName = "date")]
+        public DateTime BirthDate { get; set; }
+    }
+}
